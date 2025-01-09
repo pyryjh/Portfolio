7569c2c5069ef76e202803d56cc839568b72e398
Pyry Huhtanen <60577494+pyryjh@users.noreply.github.com>
2025-01-09 12:26:21 +0200

    Session length now available

--- customer_acquisition_analytics/utils/dataset_generation.py
+++ customer_acquisition_analytics/utils/dataset_generation.py
@@ -10,41 +10,9 @@ from IPython.display import clear_output, display, Markdown, HTML
 def hello():
     print('Hello world!')
     
-def initial_clusters_deterministic(
-        cohort_size: int,
-        shares: dict
-        ):
-    
-    shares_calc = {key: (value if value is not None else 0) for key, value in shares.items()}
-    total_provided_share = sum(shares_calc.values())
 
-    if total_provided_share > 100:
-        raise ValueError('Error: Shares exceed 100%')
 
-    if shares.get('g') is None:
-        g_share = {'g': 100 - total_provided_share}
-        shares.update(g_share)
-    
-    
-    # Calculate the number of customers per cluster
-    cluster_sizes = {key: int(cohort_size * (share / 100)) for key, share in shares.items()}
-    
-    # Handle rounding errors to ensure the total matches cohort_size
-    total_assigned = sum(cluster_sizes.values())
-    if total_assigned < cohort_size:
-        # Add the remainder to the cluster g ('free to play')
-        # largest_cluster = max(cluster_sizes, key=cluster_sizes.get)
-        cluster_sizes['g'] += cohort_size - total_assigned
-    
-    # Create the customers dictionary
-    users = {}
-    start_index = 0
-    for cluster, size in cluster_sizes.items():
-        for i in range(start_index, start_index + size):
-            users[i] = cluster
-        start_index += size
-    
-    return users
+## Production functions
 
 
 def initial_clusters(cohort_size: int, shares: dict):
@@ -226,6 +194,48 @@ def adjust_timestamps(timestamp: pd.Timestamp, start: pd.Timestamp=None, end: pd
         pass
 
     return timestamp
+
+
+
+
+## Auxiliary functions
+
+
+def initial_clusters_deterministic(
+        cohort_size: int,
+        shares: dict
+        ):
+    
+    shares_calc = {key: (value if value is not None else 0) for key, value in shares.items()}
+    total_provided_share = sum(shares_calc.values())
+
+    if total_provided_share > 100:
+        raise ValueError('Error: Shares exceed 100%')
+
+    if shares.get('g') is None:
+        g_share = {'g': 100 - total_provided_share}
+        shares.update(g_share)
+    
+    
+    # Calculate the number of customers per cluster
+    cluster_sizes = {key: int(cohort_size * (share / 100)) for key, share in shares.items()}
+    
+    # Handle rounding errors to ensure the total matches cohort_size
+    total_assigned = sum(cluster_sizes.values())
+    if total_assigned < cohort_size:
+        # Add the remainder to the cluster g ('free to play')
+        # largest_cluster = max(cluster_sizes, key=cluster_sizes.get)
+        cluster_sizes['g'] += cohort_size - total_assigned
+    
+    # Create the customers dictionary
+    users = {}
+    start_index = 0
+    for cluster, size in cluster_sizes.items():
+        for i in range(start_index, start_index + size):
+            users[i] = cluster
+        start_index += size
+    
+    return users
 
 
 def plot_hourly_distribution(df, timestamp_column='timestamp'):
